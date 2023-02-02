9d21501ec7154f4fc233b0e5d9920b9a036fdd79
Jesus Arrioja <Jarrioja2210@gmail.com>
2023-02-02 09:59:58 -0300

    added price and fix styles

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,13 +2,17 @@ import "./Item.scss";
 import { Link } from "react-router-dom";
 
 const Item = ({ item }) => {
-  const { id, name, description, stock, cat, image, author } = item;
+  const { id, name, description, stock, cat, image, author, price } = item;
   return (
     <div className='card w-25 mx-2 shadow-sm rounded' key={id}>
-      <div className='card-header'>
-        <Link to={`/detail/${id}`}> {name || "Nada"}</Link>
+      <img src={image} alt='' className='card-img-top' />
+      <div className='card-body d-grid'>
+        <h5 className='card-title '>{name}</h5>
+        <p className='card-text'> {`USD$ ${price}`}</p>
+        <Link className='stretched-link btn btn-primary' to={`/detail/${id}`}>
+          Ver detalles
+        </Link>
       </div>
-      <div className='card-body'>{description || "Nada"}</div>
     </div>
   );
 };
