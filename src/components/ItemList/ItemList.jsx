e3415f7b1ad3e5b924c0cf54da6d461b2a4f49cc
Jesus Arrioja <Jarrioja2210@gmail.com>
2023-02-01 23:21:43 -0300

    move states to ItemListContainer

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,25 +1,14 @@
 import { useEffect, useState } from "react";
-import IsLoading from "../IsLoading/IsLoading";
+
 import Item from "../Item/Item";
 import { getAllProducts } from "../../utils/getProducts";
 
-const ItemList = () => {
-  const [products, setProducts] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    getAllProducts(setProducts).finally(() => setIsLoading(false));
-  }, []);
-
+const ItemList = ({ products }) => {
   return (
     <div className='d-flex justify-content-center my-5'>
-      {isLoading ? (
-        <IsLoading />
-      ) : (
-        products.map((p) => {
-          return <Item item={p} key={p.id} />;
-        })
-      )}
+      {products.map((p) => (
+        <Item item={p} key={p.id} />
+      ))}
     </div>
   );
 };
