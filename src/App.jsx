730b6afe49ffc9eac5996a2849ce997848ec9904
Jesus Arrioja <Jarrioja2210@gmail.com>
2023-02-10 09:31:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,35 @@ import {
   Navigate,
 } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-//import "./App.scss";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import CartContainer from "./components/CartContainer/CartContainer";
-//import { useState, useEffect} from "react";
+import { CartContextProvider } from "./context/CartContext";
 
 function App() {
   return (
     <Router>
-      <div className='App'>
-        <Navbar />
-        <Routes>
-          <Route
-            path='/'
-            element={<ItemListContainer greeting='Bienvenidos' />}
-          />
-          <Route
-            path='/category/:category'
-            element={<ItemListContainer greeting='Cursos disponibles en' />}
-          />
-          <Route path='/product/:productId' element={<ItemDetailContainer />} />
-          <Route path='/cart' element={<CartContainer />} />
-          {/* <Route path='*' element={<Navigate to='/404' />} /> */}
-        </Routes>
-      </div>
+      <CartContextProvider>
+        <div className='App'>
+          <Navbar />
+          <Routes>
+            <Route
+              path='/'
+              element={<ItemListContainer greeting='Bienvenidos' />}
+            />
+            <Route
+              path='/category/:category'
+              element={<ItemListContainer greeting='Cursos disponibles en' />}
+            />
+            <Route
+              path='/product/:productId'
+              element={<ItemDetailContainer />}
+            />
+            <Route path='/cart' element={<CartContainer />} />
+            {/* <Route path='*' element={<Navigate to='/404' />} /> */}
+          </Routes>
+        </div>
+      </CartContextProvider>
     </Router>
   );
 }
